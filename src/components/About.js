3a968fdcf39cf8c7436693b787fff5bd53a704a4
wendymasese <wendy.masese@student.moringaschool.com>
2024-05-22 16:03:56 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  function bio() {
+    if (props.bio) {
+      return <p>{props.bio}</p>
+    } else {
+      return
+    }
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio()}
+      {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
